8934556d45294684fc5214dd1644c4c19f1565a1
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-09-30 19:57:54 -0300

    feat: creating axios http request

---
+++ server/src/client/clientGetUser.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+async function apiReturnUsers(params) {
+
+    try {
+      const config = {
+        url: `${process.env.GITHUB_URL}/${params.user}`,
+        method: 'GET',
+      };
+  
+    const result = await axios(config);
+    return result.data;
+    }catch(error) {
+      return error.response.data;
+    };
+  }
+  
+   module.exports = {apiReturnUsers}
